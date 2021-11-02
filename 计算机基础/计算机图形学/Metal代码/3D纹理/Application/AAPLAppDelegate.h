db625876953298c84adc53ee74500f74389ac014
774792381@qq.com <774792381@qq.com>
2021-11-02 23:34:23 +0800

    init

--- 计算机基础/计算机图形学/Metal代码/3D纹理/Application/AAPLAppDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface AAPLAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
--- 计算机基础/计算机图形学/Metal代码/3D纹理/Renderer/AAPLRenderer.h
+++
@@ -1,8 +0,0 @@
-#import <MetalKit/MetalKit.h>
-
-@interface AAPLRenderer : NSObject
-<MTKViewDelegate>
-
-- (nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)mtkView;
-
-@end
